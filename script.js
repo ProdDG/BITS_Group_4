0b634828f58e4ed5fc026b86bbe08523b4c736b0
s3979293 <s3979293@rmit.edu.vn>
2023-12-03 00:28:53 +0700

    modify index.html

--- script.js
+++ script.js
@@ -1,21 +1,59 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Wait for the DOM to be fully loaded before adding the event listener
+document.addEventListener("DOMContentLoaded", function () {
+  // Wait for the DOM to be fully loaded before adding the event listener
 
-    var button = document.querySelector('.btn-primary'); // Change this selector based on your actual button class
+  var button = document.querySelector(".btn-primary"); // Change this selector based on your actual button class
 
-    if (button) {
-        button.addEventListener('click', function () {
-            scrollToSection();
-        });
-    }
+  if (button) {
+    button.addEventListener("click", function () {
+      scrollToSection();
+    });
+  }
 });
 
 function scrollToSection() {
-    // You can replace 'targetSection' with the ID of the element you want to scroll to
-    var targetSection = document.getElementById("model");
+  var targetSection = document.getElementById("model");
 
-    if (targetSection) {
-        // Scroll to the target section smoothly
-        targetSection.scrollIntoView({ behavior: 'smooth' });
+  if (targetSection) {
+    var targetPosition = targetSection.offsetTop;
+    console.log("targetPosition:", targetPosition);
+
+    window.scrollTo({ top: targetPosition, behavior: "smooth" });
+  }
+}
+
+var scrollButton = document.getElementById("scrollButton");
+var modelElement = document.getElementById("model");
+
+function toggleScrollButton() {
+    var modelRect = modelElement.getBoundingClientRect();
+
+    // Check if the model element is NOT fully in the viewport
+    if (
+        modelRect.top < 0 ||
+        modelRect.bottom > (window.innerHeight || document.documentElement.clientHeight)
+    ) {
+        scrollButton.style.display = "none"; // Hide the button
+    } else {
+        scrollButton.style.display = "block"; // Show the button
     }
 }
+
+function handleScroll() {
+    toggleScrollButton();
+}
+
+window.addEventListener("scroll", handleScroll);
+window.addEventListener("resize", toggleScrollButton);
+
+// Initial check in case the element is already in the viewport on page load
+toggleScrollButton();
+
+// window.addEventListener('scroll', followScroll);
+
+// function followScroll() {
+//     var button = document.getElementById("scrollButton");
+//     var scrollY = window.scrollY || document.documentElement.scrollTop;
+
+//     // Adjust the top position based on your preference
+//     button.style.top = (scrollY + window.innerHeight - button.offsetHeight - 20) + "px";
+// }
