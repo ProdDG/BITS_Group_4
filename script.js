a2b22515a8e33cb1f4f8a86a6ff4f94773d4a54a
s3979293 <s3979293@rmit.edu.vn>
2023-12-22 16:21:20 +0700

    update search function

--- script.js
+++ script.js
@@ -140,3 +140,44 @@ function display(result){
 
   resultsBox.innerHTML = "<ul>" + content.join('') + "</ul>";
 }
+
+//when user clicks on the ul, it will href to the corresponding page
+// Create a mapping of list item text content to URLs
+var urlMapping = {
+  'Library': 'library.html',
+  'Classroom': 'room_detail.html',
+  'Mac Lab': 'mac-lab.html',
+  'Windows Lab': 'windows-lab.html',
+  'Duo OS Lab': 'duo-os-lab.html',
+  'Self Study Room': 'self-study-room.html',
+  'Innovation Hub': 'innovation-hub.html',
+  'Gym': 'gym.html',
+  'Reception': 'reception.html',
+  'Security': 'security.html',
+  'Design Studio': 'design-studio.html',
+  'Collaboration Study Space': 'student_space_detail.html',
+  'Teacher & Research': 'teacher-research.html',
+  'T&H Management Classroom': 'th-management-classroom.html',
+  'SAS': 'sas_detail.html',
+  'Financial Trading Lab': 'financial_detail.html',
+  'Canteen': 'canteen.html',
+  'AV Theatre': 'av_theatre_detail.html',
+  'Student Connect': 'student_connect.html',
+  'Wellbeing': 'wellbeing_details.html',
+  'Office': 'office_detail.html',
+};
+
+resultsBox.addEventListener('click', function(e){
+  if(e.target.tagName === 'LI'){
+    // Use the text content of the clicked list item to get the corresponding URL from the mapping
+    var url = urlMapping[e.target.textContent];
+    if(url){
+      window.location.href = url;
+    }
+    resultsBox.innerHTML = '';
+  }
+})
+
+
+
+
