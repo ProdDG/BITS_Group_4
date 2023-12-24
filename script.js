a7aefc917986244881a2f965f677bed7dc765124
ProdDG <s3911444@rmit.edu.vn>
2023-12-24 20:54:32 +0700

    added filter frontend + added colored models 1-5

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ function changeModel(num){
     var F2 = document.getElementsByClassName("F2");
 
     if (num == 1) {
-      modelviewer.src = "Models/1F.gltf"
+      modelviewer.src = "Models/1F-colored.gltf"
 
       for (const hspot of F1) {
         hspot.style.visibility = 'visible';
@@ -76,8 +76,8 @@ function changeModel(num){
         hspot.style.visibility = 'hidden';
       }
 
-    } else if (num == 2){
-      modelviewer.src = "Models/2F.gltf"
+    } else if (num == 2) {
+      modelviewer.src = "Models/2F-coloredNhan.gltf"
 
       for (const hspot of F1) {
         hspot.style.visibility = 'hidden';
@@ -87,97 +87,13 @@ function changeModel(num){
         hspot.style.visibility = 'visible';
       }
 
+    } else if (num == 3) {
+      modelviewer.src = "Models/3F-coloredNhan.gltf"
+    } else if (num == 4) {
+      modelviewer.src= "Models/4F-coloredNhan.gltf"
+    } else if (num == 5) {
+      modelviewer.src= "Models/5F-coloredNhan.gltf"
     }
 }
-
-let keywords = [
-  'Library',
-  'Classroom',
-  'Mac Lab',
-  'Windows Lab',
-  'Duo OS Lab',
-  'Self Study Room',
-  'Innovation Hub',
-  'Gym',
-  'Reception',
-  'Security',
-  'Design Studio',
-  'Collaboration Study Space',
-  'Teacher & Research',
-  'T&H Management Classroom',
-  'SAS',
-  'Financial Trading Lab',
-  'Canteen',
-  'AV Theatre',
-  'Student Connect',
-  'Wellbeing',
-  'Office',
-];
-
-const resultsBox = document.querySelector(".result-box");
-const inputBox = document.getElementById("input-box");
-
-inputBox.onkeyup = function(){
-  let result = [];
-  let input = inputBox.value;
-  if(input.length){
-    result = keywords.filter((keyword)=>{
-      return keyword.toLowerCase().includes(input.toLowerCase());
-    });
-    console.log(result)
-  }
-  display(result);
-
-  if(!result.length){
-    resultsBox.innerHTML = '';
-  }
-}
-
-function display(result){
-  const content = result.map((list)=>{
-    return "<li>" + list + "</li>";
-  });
-
-  resultsBox.innerHTML = "<ul>" + content.join('') + "</ul>";
-}
-
-//when user clicks on the ul, it will href to the corresponding page
-// Create a mapping of list item text content to URLs
-var urlMapping = {
-  'Library': 'library.html',
-  'Classroom': 'room_detail.html',
-  'Mac Lab': 'mac-lab.html',
-  'Windows Lab': 'windows-lab.html',
-  'Duo OS Lab': 'duo-os-lab.html',
-  'Self Study Room': 'self-study-room.html',
-  'Innovation Hub': 'innovation-hub.html',
-  'Gym': 'gym.html',
-  'Reception': 'reception.html',
-  'Security': 'security.html',
-  'Design Studio': 'design-studio.html',
-  'Collaboration Study Space': 'student_space_detail.html',
-  'Teacher & Research': 'teacher-research.html',
-  'T&H Management Classroom': 'th-management-classroom.html',
-  'SAS': 'sas_detail.html',
-  'Financial Trading Lab': 'financial_detail.html',
-  'Canteen': 'canteen.html',
-  'AV Theatre': 'av_theatre_detail.html',
-  'Student Connect': 'student_connect.html',
-  'Wellbeing': 'wellbeing_details.html',
-  'Office': 'office_detail.html',
-};
-
-resultsBox.addEventListener('click', function(e){
-  if(e.target.tagName === 'LI'){
-    // Use the text content of the clicked list item to get the corresponding URL from the mapping
-    var url = urlMapping[e.target.textContent];
-    if(url){
-      window.location.href = url;
-    }
-    resultsBox.innerHTML = '';
-  }
-})
-
-
 
 
---
+++ searchbar.js
@@ -0,0 +1,89 @@
+let keywords = [
+    'Library',
+    'Classroom',
+    'Mac Lab',
+    'Windows Lab',
+    'Duo OS Lab',
+    'Self Study Room',
+    'Innovation Hub',
+    'Gym',
+    'Reception',
+    'Security',
+    'Design Studio',
+    'Collaboration Study Space',
+    'Teacher & Research',
+    'T&H Management Classroom',
+    'SAS',
+    'Financial Trading Lab',
+    'Canteen',
+    'AV Theatre',
+    'Student Connect',
+    'Wellbeing',
+    'Office',
+  ];
+  
+  const resultsBox = document.querySelector(".result-box");
+  const inputBox = document.getElementById("input-box");
+  
+  inputBox.onkeyup = function(){
+    let result = [];
+    let input = inputBox.value;
+    if(input.length){
+      result = keywords.filter((keyword)=>{
+        return keyword.toLowerCase().includes(input.toLowerCase());
+      });
+      console.log(result)
+    }
+    display(result);
+  
+    if(!result.length){
+      resultsBox.innerHTML = '';
+    }
+  }
+  
+  function display(result){
+    const content = result.map((list)=>{
+      return "<li>" + list + "</li>";
+    });
+  
+    resultsBox.innerHTML = "<ul>" + content.join('') + "</ul>";
+  }
+  
+  //when user clicks on the ul, it will href to the corresponding page
+  // Create a mapping of list item text content to URLs
+  var urlMapping = {
+    'Library': 'library.html',
+    'Classroom': 'room_detail.html',
+    'Mac Lab': 'mac-lab.html',
+    'Windows Lab': 'windows-lab.html',
+    'Duo OS Lab': 'duo-os-lab.html',
+    'Self Study Room': 'self-study-room.html',
+    'Innovation Hub': 'innovation-hub.html',
+    'Gym': 'gym.html',
+    'Reception': 'reception.html',
+    'Security': 'security.html',
+    'Design Studio': 'design-studio.html',
+    'Collaboration Study Space': 'student_space_detail.html',
+    'Teacher & Research': 'teacher-research.html',
+    'T&H Management Classroom': 'th-management-classroom.html',
+    'SAS': 'sas_detail.html',
+    'Financial Trading Lab': 'financial_detail.html',
+    'Canteen': 'canteen.html',
+    'AV Theatre': 'av_theatre_detail.html',
+    'Student Connect': 'student_connect.html',
+    'Wellbeing': 'wellbeing_details.html',
+    'Office': 'office_detail.html',
+  };
+  
+  resultsBox.addEventListener('click', function(e){
+    if(e.target.tagName === 'LI'){
+      // Use the text content of the clicked list item to get the corresponding URL from the mapping
+      var url = urlMapping[e.target.textContent];
+      if(url){
+        window.location.href = url;
+      }
+      resultsBox.innerHTML = '';
+    }
+  })
+  
+  
