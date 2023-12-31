bd39360fd965514515ef9a5a9be57b2a1c0fd7c0
ProdDG <s3911444@rmit.edu.vn>
2023-12-31 23:24:44 +0700

    Update 31/12

--- modelzoom.js
+++ modelzoom.js
@@ -1,13 +1,3 @@
-// Model zoom function
-
-// document.getElementById("zoom-in").addEventListener("click", function(){
-//     document.getElementById("modeldisplay").removeAttribute("disable-zoom")
-// });
-
-// document.getElementById("zoom-out").addEventListener("click", function(){
-//     document.getElementById("modeldisplay").setAttribute("disable-zoom","");
-// });
-
 var modelViewer = document.querySelector('model-viewer');
 
 function zoomIn() {
